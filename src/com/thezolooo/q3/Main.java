6d4f11804129bf064e58ab2ea5a2c3708ad08726
chiragjain5june <94809590+chiragjain5june@users.noreply.github.com>
2022-03-14 13:06:14 +0530

    q3 done

--- src/com/thezolooo/q3/Main.java
+++ src/com/thezolooo/q3/Main.java
@@ -6,13 +6,13 @@ public class Main {
         String value = sc.nextLine();
         String[] array = value.split(" ");
         int a = Integer.parseInt(array[0]);
-        int b = Integer.parseInt(array[0]);
-        int i = Integer.parseInt(array[0]);
-        System.out.println(a);
+        int b = Integer.parseInt(array[1]);
+        int i = Integer.parseInt(array[2]);
+
         int s = 0;
-        while(a<b){
-            s += a;
-             a= a+2;
+        while(a<=b){
+             s = a ;
+             a = a + i;
             System.out.print(s + " ");
         }
     }
