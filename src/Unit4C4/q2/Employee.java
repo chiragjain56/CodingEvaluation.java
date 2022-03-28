5025a5fbfc72af8c6c4bf3bdbcd736c579838b6b
chiragjain5june <94809590+chiragjain5june@users.noreply.github.com>
2022-03-28 10:40:03 +0530

    q2 done

---
+++ src/Unit4C4/q2/Employee.java
@@ -0,0 +1,74 @@
+package Unit4C4.q2;
+
+public class Employee {
+    private String empId;
+    private String empName;
+    private Address address;
+    private  String email;
+    transient private String password;
+
+    public Employee() {
+    }
+
+    public Employee(String empId, String empName, Address address, String email, String password) {
+        this.empId = empId;
+        this.empName = empName;
+        this.address = address;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Employee(String chi56, String chirag, String udaipur, String email) {
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId='" + empId + '\'' +
+                ", empName='" + empName + '\'' +
+                ", address=" + address +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/Unit4C4/q2/Main.java
@@ -0,0 +1,19 @@
+package Unit4C4.q2;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Employee employee = new Employee("chi56","Chirag","Udaipur","chirag@56");
+
+        FileOutputStream fileOutputStream = new FileOutputStream("emp.txt");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(employee);
+        System.out.println("Done");
+
+        ObjectInputStream objectInputStream1 = new ObjectInputStream(new FileInputStream("emp.txt"));
+        Employee employee1 = (Employee)objectInputStream1.readObject();
+        System.out.println(employee1.toString());
+
+    }
+}
