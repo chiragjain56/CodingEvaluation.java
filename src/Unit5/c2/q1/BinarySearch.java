d23900cd8fc1e28d102b6cf453f835500a5aa7fe
chiragjain5june <94809590+chiragjain5june@users.noreply.github.com>
2022-04-18 12:22:48 +0530

    done q1 and q2

---
+++ src/Unit5/c2/q1/BinarySearch.java
@@ -0,0 +1,9 @@
+package Unit5.c2.q1;
+
+public  class BinarySearch {
+    public void binarySearch(int[] arr, int target) {
+        System.out.println("Array is sorted using Bubble sort");
+    }
+
+
+}
